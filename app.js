45898b91685de6f61a19de54ca4cb5637b5dd9e8
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-07-13 10:45:42 -0500

    missing method

--- app.js
+++ app.js
@@ -89,3 +89,14 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Visit: http://127.0.0.1:' + app.get('port'));
 });
 
+
+// Simple route middleware to ensure user is authenticated.
+// Use this route middleware on any resource that needs to be protected. If
+// the request is authenticated (typically via a persistent login session),
+// the request will proceed. Otherwise, the user will be redirected to the
+// login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
