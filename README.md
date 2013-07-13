857145e34f13f4f1296084b514a010846be8a702
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-07-13 10:37:01 -0500

    twitter auth basic setup

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This project uses the latest node 0.10.13.
 
 ### OS X
 
-We recommend you use this exact version. If you don't have node already all you have to do is run
+We recommend you use this exact version. If you don't have node already all you have to do is run:
 
 ```bash
 brew install node
@@ -13,7 +13,7 @@ brew install node
 
 If you are not sure what homebrew is. [See more information here](http://mxcl.github.io/homebrew/).
 
-If you already have a node version installed, we recommend you uninstall it and install the latest or you can use something like [nvm](https://github.com/creationix/nvm.git) to manage multiple node versions.
+If you already have a node version installed, we recommend you uninstall it and install the latest or you can use something like [nvm](https://github.com/creationix/nvm) to manage multiple node versions.
 
 If you choose to use nvm, just install node with:
 
@@ -29,4 +29,4 @@ nvm alias deafult 0.10.13
 
 ## License
 
-[MIT](http://opensource.org/licenses/MIT)
+This application is provided under the [MIT](http://opensource.org/licenses/MIT) license.
--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-var express = require('express')
-  , routes  = require('./routes')
-  , http    = require('http')
-  , path    = require('path')
-  , sass    = require('node-sass')
+var express         = require('express')
+  , routes          = require('./routes')
+  , http            = require('http')
+  , path            = require('path')
+  , sass            = require('node-sass')
+  , passport        = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , partials        = require('express-partials')
   ;
+
+var TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
+var TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the TwitterStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a token, tokenSecret, and Twitter profile), and
+// invoke a callback with a user object.
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Twitter profile is returned to
+      // represent the logged-in user. In a typical application, you would want
+      // to associate the Twitter account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.use(partials()); // ejs partials
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(express.cookieParser(process.env.COOKIE_SECRET || 'barcampz'));
 app.use(express.session());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,9 +72,20 @@ if ('development' == app.get('env')) {
   );
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { user: req.user, layout: 'layout' });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user, layout: 'layout' });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, layout: 'layout' });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  console.log('Visit: http://localhost:' + app.get('port'));
+  console.log('Visit: http://127.0.0.1:' + app.get('port'));
 });
+
