90724cb338ce87054083ddef348e4ad37b23d55d
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-07-13 23:32:02 -0500

    make searcher work or whatever

--- script/searcher.js
+++ script/searcher.js
@@ -12,27 +12,14 @@ var tags = ['#barcampnola', '#bearcamp', '#bearcampnola', '#nolatech'];
 
 var search_opts = { count: 100 };
 
-Settings.fetch(function (err, settings) {
-  var count  = 100
-    , max_id = null;
-
-  async.until(
-    function () { count < 100 },
-    function (callback) {
-      if (max_id) search_opts.max_id = max_id;
-
-      twitter.search(tags.join(' OR '), search_opts, function (err, data) {
-        count = data.search_metadata.count;
-        max_id = data.search_metadata.max_id;
-        console.log(data.search_metadata)
-        data.statuses.forEach(function (twitter_obj) {
-          var tweet = Tweet.build(twitter_obj);
-          tweet.save(console.log);
-        });
-        callback();
-      });
-  }, function () {
-    process.exit(0);
+twitter
+  .search(tags.join(' OR '), search_opts, function (err, data) {
+    count = data.search_metadata.count;
+    max_id = data.search_metadata.max_id;
+    console.log(data.search_metadata)
+    data.statuses.forEach(function (twitter_obj) {
+      var tweet = Tweet.build(twitter_obj);
+      tweet.save(console.log);
+    });
   });
-});
 
