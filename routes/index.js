e5d3061d19bad3745751ad09db2faf80484473ca
Benjamin Eckel <benjamin.eckel@gmail.com>
2013-07-14 16:26:25 -0500

    top five filtered

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 var Tweet = require('../models').Tweet;
 
+var tags = ["#nolabulls",
+            "#totc",
+            "#nolacommunity",
+            "#neworleanspelicans",
+            "#nola",
+            "#essencefest",
+            "#nolacrawl",
+            "#NewOrleans",
+            "#whodat",
+            "#nolaproblems",
+            "#onlyinnola",
+            "#barcampnola"
+           ]
+
 exports.index = function(req, res){
   res.render('index', { title: 'Barcamp6' });
 };
 
 exports.topFive = function (req, res) {
-  Tweet.tagStats(function (err, stats) {
+  Tweet.topFiveTagsFiltered(tags, function (err, stats) {
     res.json({ stats: stats });
   });
 };
