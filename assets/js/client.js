ed49fc1d8102bb7f8c04237650a09a6afb2ac2e6
H. Cole Wiley <hcwiley@gmail.com>
2013-07-14 12:45:34 -0500

    sockets posting to index page when they get stuff. also reload page if server restarts

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,12 +1,24 @@
 //=require socket.io
 //=require jquery
 
+didDisconnect = false
+
 $(window).ready(function(){
 
   socket = io.connect()
 
   socket.on("connection", function(data){
-    socket.emit("ripp-it", "croak");
+    if( didDisconnect ) {
+      window.location = window.location.pathname;
+    } else {
+      var li = $("<li>"+data+"</li>");
+      $("#socket-stream").append(li);
+      socket.emit("ripp-it", "croak");
+    }
+  });
+
+  socket.on("disconnect", function(data){
+    didDisconnect = true;
   });
   
 });
